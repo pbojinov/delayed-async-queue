3943c551f453f624b8645bd126b0c10431754623
Petar Bojinov <petarbojinov@gmail.com>
2016-03-02 14:02:58 -0800

    got the queue working with an iife

--- daq.js
+++ daq.js
@@ -1,3 +1,13 @@
+
+// Third-Party Code
+window._daq = window._daq || [];
+_daq.push([() => {
+  console.log('hi there queue, thanks for running me!');
+}]); // test iife
+// _daq.push(['regularFunctionName', 'do-something']); // test regular function
+// _teq.push(['App.namespacedfunction', 'some-something-else', 'more data']); // test namespaced function
+
+
 /**
  * Delayed Async Queue
  */
@@ -18,16 +28,47 @@ class DelayedAsyncQueue {
   _processQueue() {
     console.log('queue with length: ', this.queue.length);
     if (this.queue.length > 0) {
-      isProcessing = true;
+      this.isProcessing = true;
       for (var i = 0; i < this.queue.length; i++) {
         if (this.queue[i]) {
           // send off item to get processed
-          // apply(this.queue[i]);
+          this._apply(this.queue[i]);
         }
       }
       this.queue.length = 0; // empty queue
       this.isProcessing = false;
     }
+  }
+
+  // @private
+  _apply() {
+    let func = null;
+    let parameterArray = null;
+    for (let i = 0; i < arguments.length; i += 1) {
+      parameterArray = arguments[i];
+      func = parameterArray.shift();
+      //   console.log(func);
+      //   console.log(parameterArray);
+      // console.log('typeof func', typeof func)
+      if (typeof func === 'string') {
+        this._executeFunctionByName(func, window, parameterArray)
+      } else if (typeof func === 'function') {
+        func.apply(parameterArray);
+      }
+    }
+  }
+
+
+  // @private
+  // http://stackoverflow.com/a/359910/907388
+  _executeFunctionByName(functionName, context) {
+    let args = [].slice.call(arguments).splice(2);
+    let namespaces = functionName.split('.');
+    let func = namespaces.pop();
+    for (let i = 0; i < namespaces.length; i++) {
+      context = context[namespaces[i]];
+    }
+    return context[func].apply(this, args);
   }
 
   ////////////////////////////
@@ -48,19 +89,41 @@ class DelayedAsyncQueue {
     return window.clearInterval(this.queueInterval);
   }
 
-  get length() {
-    return this.queue.length;
-  }
-
-  get intervalTimer() {
-    return this.intervalTimer;
-  }
-
-  get isProcessing() {
-    return this.isProcessing;
-  }
+  // get length() {
+  //   return this.queue.length;
+  // }
+  //
+  // get intervalTimer() {
+  //   return this.intervalTimer;
+  // }
+  //
+  // get isProcessing() {
+  //   return this.isProcessing;
+  // }
 
 }
 
-let queue = new DelayedAsyncQueue();
+// import DelayedAsyncQueue from 'delayedasyncqueue';
+let queue = new DelayedAsyncQueue(queue = window._daq);
 queue.start();
+
+////////////////////////////
+/// Sample public api that will be called by the queue
+////////////////////////////
+var App = App || {};
+App.namespacedfunction = function() {
+  // this function is being called directly but through function queue
+  var args = [].slice.call(arguments[0]);
+  name = args[0];
+  data = args[1];
+  console.log('running: ', name);
+  console.log('with data: ', data);
+}
+function regularFunctionName() {
+  // this function is being called directly but through function queue
+  var args = [].slice.call(arguments[0]);
+  name = args[0];
+  data = args[1];
+  console.log('running experiment: ', name);
+  console.log('with data: ', data);
+}
